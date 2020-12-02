e4fc1355e076f200738973c830e1595f0534a0c1
Huzefa Motorwala <65020217+HuzefaMotorwala@users.noreply.github.com>
2020-12-02 20:56:38 +0530

    Update Palindrome_Checker.js

--- Palindrome_Checker.js
+++ Palindrome_Checker.js
@@ -1,1 +1,16 @@
+function palindrome(str) {
+  var newstr=str.replace(/\W+|_/g,"").toLowerCase();
+  let reversestr=newstr.split("").reverse().join("").toLowerCase();
+  console.log(newstr);
+  console.log(reversestr);
+  if(newstr!==reversestr){
+    return false;
+  }
+  return true;
+  
+  //console.log(newstr);
+}
 
+
+
+palindrome("_eye");
